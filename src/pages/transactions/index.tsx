d9ed72b2189bf5fe595d8c35172da2d0dd53d877
pmartinsdev <martins20.tech@gmail.com>
2023-01-07 18:53:18 -0300

    feat(transaction): create table component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,12 +1,40 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
+import {
+  PriceHightlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Development website</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000,00</PriceHightlight>
+              </td>
+              <td>Sell</td>
+              <td>04/13/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHightlight variant="outcome"> - R$ 59,00</PriceHightlight>{" "}
+              </td>
+              <td>Food</td>
+              <td>04/13/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
