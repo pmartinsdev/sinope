0d5b417954eafa382d8c42b583128c282cfe8e80
pmartinsdev <martins20.tech@gmail.com>
2023-01-07 18:53:18 -0300

    style(search-form): create search form container

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
+import { SearchForm } from "./components/search-form";
 import {
   PriceHightlight,
   TransactionsContainer,
@@ -13,6 +14,8 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
@@ -27,7 +30,6 @@ export function Transactions() {
             <tr>
               <td width="50%">Hamburguer</td>
               <td>
-                {" "}
                 <PriceHightlight variant="outcome"> - R$ 59,00</PriceHightlight>
               </td>
               <td>Food</td>
