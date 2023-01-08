cbd99e4e4da48d055a37723a2dd1a0db252485f7
pmartinsdev <martins20.tech@gmail.com>
2023-01-08 10:56:32 -0300

    feat(date-formatter): add 2 digits into day and month

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,10 @@
 const defaultLocale = "en";
 
-export const dateFormatter = new Intl.DateTimeFormat(defaultLocale);
+export const dateFormatter = new Intl.DateTimeFormat(defaultLocale, {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+});
 
 export const priceFormatter = new Intl.NumberFormat(defaultLocale, {
   style: "currency",
